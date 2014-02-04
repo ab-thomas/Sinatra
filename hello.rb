5e27d4a54342029d78665c01f91790b943308607
Abraham Thomas <dysruptmedia@gmail.com>
2014-02-04 12:11:19 +0000

    Berry the cat added

--- hello.rb
+++ hello.rb
@@ -1,7 +1,24 @@
 require 'sinatra'
 
+get '/' do
+  'hello!'
+end
+
+get '/secret' do
+  'This is a secret page'
+end
+
 get '/hello' do
   @visitor = params[:name]
   erb :index
 end
+
+get '/berry' do
+  erb :berry
+end
+
  
+# get '/' do
+#   @name = %w(Amigo Oscar Viking).sample
+#   erb :index
+# end
